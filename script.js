e6e8309f56ee7afb2a402f2f54ebd6dc7a090d54
mbade1 <mbade314@gmail.com>
2020-09-16 21:47:01 -0500

    Animated scroll events with jquery

--- script.js
+++ script.js
@@ -1,46 +1,29 @@
-let enter = document.querySelector('#enter')
-let bg = document.querySelector('.bg')
-let enterBar = document.querySelector('.enter-bar')
-let about = document.querySelector('.about')
-let projects = document.querySelector('.projects')
-let contact = document.querySelector('.contact')
-let resume = document.querySelector('.resume')
-let body = document.querySelector('body')
-let navbar = document.querySelector('.navbar')
-let headerAfterEnter = document.querySelector('.header-after-enter')
-let sectionHeader = document.getElementsByClassName('section-header')
-let headerInformation = document.querySelector('.header-information')
-let stacks = document.querySelector('.stacks')
-let sectionHeaderContact = document.querySelector('.section-header-contact')
-let footer = document.querySelector('.footer')
+$("#about-link").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#about").offset().top
+  }, 1000);
+});
 
-enter.addEventListener('click', function(e) {
-  bg.setAttribute("data-aos", "fade");
+$("#projects-link").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#projects").offset().top
+  }, 1200);
+});
 
-    let styles = {
-        "height": `70%`,
-        "background": `linear-gradient(rgba(255,255,255,.2), rgba(255,255,255, .2)), url("pictures/alaska.jpeg")`,
-        "background-color": "rgb(77, 75, 75)",
-        "background-position": "center",
-        "background-repeat": "no-repeat",
-        "background-size": "cover",
-      };
-    Object.assign(bg.style, styles);
-    enterBar.style.display = 'none';
-    headerInformation.style.display = 'inline-block';
-    navbar.style.display = 'inline-block';
-    about.style.display = 'inline-block';
-    projects.style.display = 'inline-block';
-    stacks.style.display = 'inline-block';
-    contact.style.display = 'inline-block';
-    sectionHeaderContact.style.display = 'inline-block';
-    footer.style.display = 'inline-block';
-    displaySectionHeader()
-})
+$("#stacks-link").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#stacks").offset().top
+  }, 2000);
+});
 
-function displaySectionHeader() {
-  for (let i = 0; i < sectionHeader.length; i++) {
-    sectionHeader[i].style.display = 'inline-block'
-  }
+$("#blog-link").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#blog").offset().top
+  }, 2000);
+});
 
-}
+$("#contact-link").click(function() {
+  $('html, body').animate({
+      scrollTop: $("#contact").offset().top
+  }, 2000);
+});
